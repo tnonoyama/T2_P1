071667652af21e9ccf815efdb126ee4bf8a6778b
tnonoyama <30341881+tnonoyama@users.noreply.github.com>
2018-06-09 17:25:38 +0900

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+  
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +36,18 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P_ * Ht * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,44 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  // recover the polar coordinates
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float ro = sqrt(px * px + py * py);
+  float theta = atan2(py, px);
+  float ro_dot = (px * vx + py * vy) / ro;
+  
+  if (fabs(ro) < 0.0001) {
+    ro_dot = 0;
+  } else {
+    ro_dot = ro_dot;
+  }
+  
+  VectorXd z_pred(3);
+  z_pred << ro, theta, ro_dot;
+
+  VectorXd y = z - z_pred;
+  
+  /* angle Normalization */
+  while (y[1] > M_PI) {
+      y[1] -= 2.* M_PI;
+  }
+  while (y[1] < -M_PI) {
+    y[1] += 2.* M_PI;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+  
 }
